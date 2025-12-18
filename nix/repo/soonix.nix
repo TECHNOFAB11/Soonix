e9860d65a0123af948c1a2da65664a6d481efe05
technofab <admin@technofab.de>
2025-12-18 09:02:46 +0100

    chore: add renovate

--- nix/repo/soonix.nix
+++ nix/repo/soonix.nix
@@ -9,6 +9,26 @@ in
   (soonix.make {
     hooks = {
       ci = ci.soonix;
+      renovate = {
+        output = ".gitlab/renovate.json5";
+        data = {
+          extends = ["config:recommended"];
+          postUpgradeTasks.commands = [
+            "nix-portable nix run .#soonix:update"
+          ];
+          lockFileMaintenance = {
+            enabled = true;
+            extends = ["schedule:monthly"];
+          };
+          nix.enabled = true;
+          gitlabci.enabled = false;
+        };
+        hook = {
+          mode = "copy";
+          gitignore = false;
+        };
+        opts.format = "json";
+      };
       test = {
         output = "test.yaml";
         generator = "nix";
